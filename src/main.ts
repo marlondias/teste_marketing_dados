f195a7b26b71fd54822899fbf6a05639aeed6166
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-05-25 17:58:47 -0300

    feat: Incluido suporte ao Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Marketing API')
+    .setDescription('An API for Marketing needs.')
+    .setVersion('1.0')
+    .addTag('marketing')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('openapi', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
