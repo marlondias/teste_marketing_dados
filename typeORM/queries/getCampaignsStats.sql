ac3a8670ad9e3f4f78b71590a40fd9527ff4ddf7
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-05-26 15:50:09 -0300

    feat: Incluido SQL cru para obter estatisticas de campanhas

---
+++ typeORM/queries/getCampaignsStats.sql
@@ -0,0 +1,33 @@
+WITH
+campanhas_com_totais AS (
+	SELECT
+		c.id AS campanha_id, 
+		c.orcamento,
+		SUM(m.custo_por_clique * m.cliques) AS custo_total,
+		SUM(m.conversoes) * (:valor_produto)::numeric AS receita,
+		COUNT(m.id) AS quant_metricas,
+		SUM(m.impressoes) AS total_impressoes,
+		SUM(m.cliques) AS total_cliques, 
+		SUM(m.conversoes) AS total_conversoes
+	FROM campanhas c 
+	LEFT JOIN metricas m ON c.id = m.campanha_id
+	GROUP BY c.id
+),
+campanhas_com_calculos AS (
+	SELECT
+		c.campanha_id,
+		c.orcamento,
+		c.custo_total,
+		(c.custo_total / NULLIF(c.orcamento, 0)) * 100 AS taxa_consumo_orcamento,
+		(c.total_cliques::numeric / NULLIF(c.total_impressoes, 0)) * 100 AS ctr,
+		(c.total_conversoes::numeric / NULLIF(c.total_cliques, 0)) * 100 AS taxa_conversao,
+		(c.custo_total / NULLIF(c.total_conversoes, 0)) AS cpa,
+		(c.receita / NULLIF(c.custo_total, 0)) * 100 AS roas,
+		((c.receita - c.custo_total) / NULLIF(c.custo_total, 0)) * 100 AS roi
+	FROM campanhas_com_totais c
+)
+
+SELECT
+	c.*,
+	(roi = MAX(roi) OVER())::boolean AS is_melhor_roi
+FROM campanhas_com_calculos c
