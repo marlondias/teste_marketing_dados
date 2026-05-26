298f468f4f4bcb6af830f41bb4bdc15a49bf3196
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-05-25 23:40:59 -0300

    feat: Implementado MetricService

--- src/services/MetricsService.ts
+++ src/services/MetricsService.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Metrica } from 'typeORM/entities/Metrica';
 
 @Injectable()
 export class MetricsService {
-  //TODO
+  constructor(
+    @InjectRepository(Metrica)
+    private readonly metricaRepository: Repository<Metrica>,
+  ) {}
+
+  async getAllByCampaign(campaignId: number): Promise<Metrica[]> {
+    return this.metricaRepository.find({
+      where: { campanha_id: campaignId },
+      order: { data_metrica: 'ASC' },
+    });
+  }
 }
